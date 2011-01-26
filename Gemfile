76a8a72cb2882566463e90ac4a4ea07135d5d824
Chris Boyle <chris@bbck.net>
2011-01-25 23:46:27 -0500

    Added hoptoad for error tracking

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'armory',               :git => 'git://github.com/bbck/armory.git'
 gem 'resque',               '~> 1.10.0'
 gem 'resque-status',        '~> 0.2.1'
 gem 'SystemTimer',          '~> 1.2.1'
+gem 'hoptoad_notifier',     '~> 2.4.2'
+gem 'heroku',               '~> 1.17.10'
 
 group :test, :development do
   gem 'rspec-rails',        '~> 2.4.1'
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,10 @@
+require 'resque/failure/hoptoad'
 require 'resque/job_with_status'
 
 Resque.redis = ENV["REDISTOGO_URL"]
-Resque::Status.expire_in = (24 * 60 * 60) # 24hrs in seconds
+Resque::Status.expire_in = (4 * 60 * 60) # 4hrs in seconds
+
+Resque::Failure::Hoptoad.configure do |config|
+  config.api_key = ENV["HOPTOAD_API_KEY"]
+  config.secure = true
+end
