6857f0a6a818e390fb5afe5961fffdae9bdae7d7
Chris Boyle <chris@bbck.net>
2011-05-31 20:32:00 -0400

    Remove devise (way too early for that) and misc spring cleaning.

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-  devise :omniauthable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Achiev::Application.routes.draw do
 
-  devise_for :users
-
   root :to => "pages#index"
 end
