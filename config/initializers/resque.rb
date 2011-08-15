0ba4a6ba96bf5e9a54708ced57d28c7bd13c0fb2
Chris Boyle <chris@bbck.net>
2011-08-15 11:03:04 -0400

    Leftover hoptoad stuff.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,8 +1,1 @@
-require 'resque/failure/hoptoad'
-
 Resque.redis = ENV["REDISTOGO_URL"]
-
-Resque::Failure::Hoptoad.configure do |config|
-  config.api_key = ENV["HOPTOAD_API_KEY"]
-  config.secure = true
-end
