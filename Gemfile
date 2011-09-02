e0ce12a8ac95180a4a4df470455063d70cbc9521
Chris Boyle <chris@bbck.net>
2011-09-02 08:25:30 -0400

    Rails 3.1 updates

--- Gemfile
+++ Gemfile
@@ -1,16 +1,19 @@
 source 'http://rubygems.org'
 
-gem 'rails',                '3.1.0'
+gem 'rails', '3.1.0'
 gem 'mysql2'
-gem 'sass-rails'
-gem 'coffee-script'
-gem 'uglifier'
 gem 'jquery-rails'
 gem 'battlenet', :git => 'git://github.com/MrMarvin/battlenet.git'
 gem 'resque'
 gem 'unicorn'
 gem 'capistrano'
 gem 'kaminari'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
 
 group :test, :development do
   gem 'rspec-rails'
--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,17 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 APP_CONFIG = YAML.load(File.read(File.expand_path('../config.yml', __FILE__)))[Rails.env]
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require *Rails.groups(:assets => %w(development test))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module Achiev
   class Application < Rails::Application
@@ -45,5 +49,8 @@ module Achiev
 
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
