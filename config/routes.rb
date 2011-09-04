7307b397fa3a62058297ec6d648b68418f94c483
Chris Boyle <chris@bbck.net>
2011-09-04 08:25:18 -0400

    Link to realm pages in ranks. Remove unneeded topbar links.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Achiev::Application.routes.draw do
 
-  match "/characters/:region/:realm/:name" => "characters#show"
-  match "/characters/:region/:realm" => "characters#index"
-  match "/characters/:region" => "characters#index"
-  match "/characters" => "characters#index"
+  match "/characters/:region/:realm/:name" => "characters#show", :as => :character
+  match "/characters/:region/:realm" => "characters#index", :as => :characters_realm
+  match "/characters/:region" => "characters#index", :as => :characters_region
+  match "/characters" => "characters#index", :as => :characters_all
 
   root :to => "pages#index"
 end
