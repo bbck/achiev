2461e3eac08cae5446102967ab576dc66a0b7479
Chris Boyle <chris@bbck.net>
2011-09-04 10:10:08 -0400

    Adjusted redis-store config and added it to session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Achiev::Application.config.session_store :cookie_store, :key => '_achiev_session'
+Achiev::Application.config.session_store :redis_session_store, { :db => 0, :namespace => "session" }
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
