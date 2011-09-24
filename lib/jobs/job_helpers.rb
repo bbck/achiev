fbae63ad84b47a4cf1334c2eb8ab0fdc77846789
Chris Boyle <chris@bbck.net>
2011-09-24 17:55:05 -0400

    Forgot method arguments.

--- lib/jobs/job_helpers.rb
+++ lib/jobs/job_helpers.rb
@@ -4,7 +4,7 @@ module JobHelpers
     Battlenet::API::Character.profile(name, realm, ["guild"])
   end
   
-  def guild_from_armory
+  def guild_from_armory(region, realm, name)
     Battlenet::API.set_option(:region, region.downcase.to_sym)
     Battlenet::API::Guild.profile(name, realm, ["members"])
   end
