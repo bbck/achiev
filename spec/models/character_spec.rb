88e980fc7d54c5499c7113127b306bd6fb1c1fca
Chris Boyle <chris@bbck.net>
2011-10-10 06:38:33 -0400

    Array shorthand isn't separated by commas.

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -10,4 +10,9 @@ describe Character do
     character = Character.new(region: "uk", realm: "Kil'jaeden", name: "Thehermit")
     character.should_not be_valid
   end
+  
+  it "should accept a valid region" do
+    character = Character.new(region: "us", realm: "Kil'jaeden", name: "Thehermit")
+    character.should be_valid
+  end
 end
