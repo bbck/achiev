c4248ae8386599de266db4c19bf196fc3e5e4ffc
Chris Boyle <chris@bbck.net>
2011-12-16 15:15:39 -0500

    Finally a battlenet API app key!

--- lib/jobs/job_helpers.rb
+++ lib/jobs/job_helpers.rb
@@ -1,11 +1,11 @@
 module JobHelpers
   def character_from_armory(region, realm, name)
-    armory = Battlenet.new(region.downcase.to_sym)
+    armory = Battlenet.new(region.downcase.to_sym, APP_CONFIG["bnet_public"], APP_CONFIG["bnet_private"])
     armory.character(realm, name, :fields => "guild")
   end
   
   def guild_from_armory(region, realm, name)
-    armory = Battlenet.new(region.downcase.to_sym)
+    armory = Battlenet.new(region.downcase.to_sym, APP_CONFIG["bnet_public"], APP_CONFIG["bnet_private"])
     armory.guild(realm, name, :fields => "members")
   end
   
