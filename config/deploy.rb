5deeebfe1df793255908817623693fd805db0e59
Chris Boyle <chris@bbck.net>
2012-01-15 13:26:20 -0500

    Capistrano should finally work correctly now.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,10 +10,12 @@ set :use_sudo,         false
 set :user,             "achiev"
 set :deploy_to,        "/home/#{user}"
 set :unicorn_pid,      "#{shared_path}/pids/unicorn.pid"
+set :unicorn_config,   "#{current_path}/config/unicorn.rb"
 set :ssh_options,      { :forward_agent => true }
 set :whenever_command, "bundle exec whenever"
-
-default_environment["PATH"] = "/home/achiev/.rbenv/shims:/home/achiev/.rbenv/bin:/usr/local/bin:/usr/bin:/bin"
+set :default_environment, {
+  'PATH' => "/home/achiev/.rbenv/shims:/home/achiev/.rbenv/bin:$PATH"
+}
 
 role :web, "achiev.bbck.net"
 role :app, "achiev.bbck.net"
@@ -28,17 +30,17 @@ namespace :deploy do
   
   desc "Restart Unicorn"
   task :restart, :except => { :no_release => true } do
-    run "kill -s USR2 `cat #{shared_path}/pids/unicorn.pid`"
+    run "kill -s USR2 `cat #{unicorn_pid}`"
   end
 
   desc "Start Unicorn"
   task :start, :except => { :no_release => true } do
-    run "cd #{current_path} ; bundle exec unicorn_rails -c #{release_path}/config/unicorn.rb -D"
+    run "cd #{current_path} && bundle exec unicorn_rails -c #{unicorn_config} -D"
   end
 
   desc "Stop Unicorn"
   task :stop, :except => { :no_release => true } do
-    run "kill -s QUIT `cat #{shared_path}/pids/unicorn.pid`"
+    run "kill -s QUIT `cat #{unicorn_pid}`"
   end
 end
 
