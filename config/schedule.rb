ea685b735a2c475c0040ebb3f7c2cae337108917
Chris Boyle <chris@bbck.net>
2012-01-20 19:34:05 -0500

    Start unicorn and resque on reboot.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,8 @@
 every :hour do
   command "/home/achiev/current/script/failed"
 end
+
+every :reboot do
+  command "cd /home/achiev/current && bundle exec unicorn_rails -c /home/achiev/current/config/unicorn.rb -D"
+  command "cd /home/achiev/current && RAILS_ENV=production BACKGROUND=yes QUEUES=character,guild PIDFILE=/home/achiev/shared/pids/resque.pid bundle exec rake resque:work"
+end
