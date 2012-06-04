ed22e6c66fbc36416967b24773180d45f65630dc
Chris Boyle <chris@bbck.net>
2012-06-03 22:26:32 -0400

    Delete characters and guilds that 404.

--- lib/jobs/failed_hooks.rb
+++ lib/jobs/failed_hooks.rb
@@ -4,11 +4,18 @@ module FailedHooks
       Resque.enqueue self, *args
       puts "API limit reached. Will retry in 30 minutes..."
       sleep 1800
-    end
-    if e.code == 500
+    elsif e.code == 500
       Resque.enqueue self, *args
       puts "Blizzard API error. Will retry in 5 minutes..."
       sleep 500
+    elsif e.code == 404
+      if self.name == "CharacterJob"
+        character = Character.find_by_region_and_realm_and_name(*args)
+        character.destroy unless character.nil?
+      elsif self.name == "GuildJob"
+        guild = Guild.find_by_region_and_realm_and_name(*args)
+        guild.destroy unless guild.nil?
+      end
     end
   end
 end
